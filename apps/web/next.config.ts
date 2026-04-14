9d5f7b32649b043d77db06faadbb4abfd3f784af
AnjanyKumarJaiswal <anjany.jaiswal2005@gmail.com>
2026-04-14 06:20:56 +0530

    fix: shared package fixed resolved

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  transpilePackages: ["@aegiscode/shared"],
+};
 
 export default nextConfig;
